ad204735f3470d3f6981ec5cbcb027c98085c411
Alexey Kovtunovich <kovtalex@gmail.com>
2021-12-14 00:32:04 +0300

    add HW 10

---
+++ HW10/ionice.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap 'trap " " SIGTERM; kill 0; wait' SIGINT SIGTERM
+
+rm -f /tmp/ioniceHiPri.tmp /tmp/ioniceLowPri.tmp
+cp /dev/null /tmp/ionice.log
+
+lowPriority()
+{
+  echo "$(date) - Start dd with low IO priority." >> /tmp/ionice.log
+  ionice -c2 -n7 dd if=/dev/zero of=/tmp/ioniceLowPri.tmp bs=64M count=80 oflag=dsync > /dev/null 2>&1
+  echo "$(date) - Stop dd with low IO priority." >> /tmp/ionice.log
+}
+
+hiPriority() {
+  echo "$(date) - Start dd with hi IO priority." >> /tmp/ionice.log
+ gionice -c1 -n0 dd if=/dev/zero of=/tmp/ioniceHiPri.tmp bs=64M count=80 oflag=dsync > /dev/null 2>&1
+  echo "$(date) - Stop dd with hi IO priority." >> /tmp/ionice.log
+}
+
+lowPriority &
+hiPriority &
+
+wait
+
+cat /tmp/ionice.log
